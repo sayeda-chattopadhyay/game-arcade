c1706c4361e899f2b1f8610fbf8e09aaf05858ea
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2024-02-26 15:31:35 +0100

    commit out proxy function

--- proxy.js
+++ proxy.js
@@ -1,20 +1,21 @@
 // proxy.js
 
-exports.handler = async (event, context) => {
-    const url = "http://api.quotable.io/random";
+// exports.handler = async (event, context) => {
+//     const url = "http://api.quotable.io/random";
     
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
+//     try {
+//         const response = await fetch(url);
+//         const data = await response.json();
+//         console.log("data", data);
         
-        return {
-            statusCode: 200,
-            body: JSON.stringify(data)
-        };
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: "Internal Server Error" })
-        };
-    }
-};
+//         return {
+//             statusCode: 200,
+//             body: JSON.stringify(data)
+//         };
+//     } catch (error) {
+//         return {
+//             statusCode: 500,
+//             body: JSON.stringify({ error: "Internal Server Error" })
+//         };
+//     }
+// };
